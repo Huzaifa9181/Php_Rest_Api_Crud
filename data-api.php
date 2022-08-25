da5b032422f9603453d9c0c5fd965e1860c0dbbd
Huzaifa Ahmed <91969198+Huzaifa9181@users.noreply.github.com>
2022-08-25 23:38:10 +0500

    Fetch all data for database

---
+++ data-api.php
@@ -0,0 +1,18 @@
+<?php
+
+// headers
+
+header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+
+    include "../database.php";
+    $sql = "SELECT * FROM `data`";
+    $result = mysqli_query($conn,$sql);
+    if(mysqli_num_rows($result) > 0){
+        $data = mysqli_fetch_all($result,MYSQLI_ASSOC);
+        echo $json = json_encode($data,JSON_PRETTY_PRINT);
+    }else{
+        $arr = array("message" => "No result found.", "Status" => false);
+        echo json_encode($arr);
+    }
+?>
