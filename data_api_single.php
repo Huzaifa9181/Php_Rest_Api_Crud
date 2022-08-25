d39ee829e0b95efe7e269eb498ff504f5f8f5221
Huzaifa Ahmed <91969198+Huzaifa9181@users.noreply.github.com>
2022-08-25 23:40:30 +0500

    fetch single data api

---
+++ data_api_single.php
@@ -0,0 +1,21 @@
+<?php
+
+// headers
+
+header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+
+$data = json_decode(file_get_contents("php://input"),true);
+
+$id = $data['id'];
+    include "../database.php";
+    $sql = "SELECT * FROM `data` Where id = {$id}";
+    $result = mysqli_query($conn,$sql);
+    if(mysqli_num_rows($result) > 0){
+        $data = mysqli_fetch_all($result,MYSQLI_ASSOC);
+        echo $json = json_encode($data,JSON_PRETTY_PRINT);
+    }else{
+        $arr = array("message" => "No result found.", "Status" => false);
+        echo json_encode($arr);
+    }
+?>
